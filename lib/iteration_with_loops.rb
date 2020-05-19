21a4d0afe6dc183a30e314055c184dee1c059338
Bree Warren <ambreea.warren@gmail.com>
2020-05-19 00:27:11 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,7 +3,7 @@ def find_min_in_nested_arrays(src)
   row_index = 0
   while row_index < src.count do
     element_index = 0
-    lowest_temperature_element = ""
+    lowest_temperature_element = 0
     while element_index < src[row_index].count do
       ############
       
