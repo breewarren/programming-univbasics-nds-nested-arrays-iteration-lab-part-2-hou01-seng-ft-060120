cbcc820dcdafc522980614a90c207e72bd787709
Bree Warren <ambreea.warren@gmail.com>
2020-05-19 00:22:51 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,14 +1,30 @@
 def find_min_in_nested_arrays(src)
-  array_of_daily_temperatures = []
+  low_temperature_for_each_day = []
   row_index = 0
   while row_index < src.count do
     element_index = 0
+    while element_index < src[row_index].count do
+      ############
+      
     
 end
 
   # src will be an array of arrays of integers
   # Produce a new Array that contains the smallest number of each of the nested arrays
   
+  def find_even_values(src)
+  row_index = 0
+  while row_index < src.count do
+    element_index = 0
+    while element_index < src[row_index].count do
+      if src[row_index][element_index].even?
+        p src[row_index][element_index]
+        end
+        element_index += 1
+        end
+        row_index += 1
+        end
+        end
   
 outer_results = []
 row_index = 0
