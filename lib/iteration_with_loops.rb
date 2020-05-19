5cacd0739e3e7a3f5dee648fd9ed9d2b2ad0b89d
Bree Warren <ambreea.warren@gmail.com>
2020-05-19 00:38:48 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ def find_min_in_nested_arrays(src)
     element_index = 0
     min_integer_element = 0
     while element_index < src[row_index].count do
-      if src[row_index][element_index].length < min_integer_element
+      if src[row_index][element_index] < min_integer_element.length
         min_integer_element = src[row_index][element_index]
       end
       element_index += 1
